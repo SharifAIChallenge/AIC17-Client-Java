7b564c6024b80fe3668c37e04a9dbd7925081c60
Future <mn7697np@gmail.com>
2017-02-10 17:25:55 +0330

    Adding Constants Classes And Enums

---
+++ src/client/model/Constants.java
@@ -0,0 +1,27 @@
+package client.model;
+
+/**
+ * Created by Future on 2/10/17.
+ */
+public interface Constants {
+    double getFoodProb();
+    double getTrashProb();
+    double getSlipperProb();
+    int getTurnTimeout();
+    int getColorCost();
+    int getSickCost();
+    int getUpdateCost();
+    int getDetMoveCost();
+    int getKillQueenScore();
+    int getKillBothQueenScore();
+    int getKillBeetleScore();
+    int getBeetleFoodScore();
+    int getWingedBeetleFoodScore();
+    int getSickLifeTime();
+    double getPowerRatio();
+    double getEndRatio();
+    int getDisobeyConstant();
+    int getFoodValidTime();
+    int getTrashValidTime();
+    int getSlipperValidTime();
+}
